87e12d354ec241e8e433d2116a89bf058d1837e7
Fabio Teixeira - EIN - TSI <fabio5413@ica.com>
2025-06-11 15:04:12 -0300

    first commit

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,7 @@
 export function Footer(){
     return (
             <footer className="fixed bottom-0 w-full items-center text-center py-4 ">
-                <p className="text-sm text-white">© 2025 Pixel Knights</p>
+                <p className="text-sm text-black">© 2025 Pixel Knights</p>
             </footer>
     )
 }
